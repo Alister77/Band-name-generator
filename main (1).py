ec42d07aea25af9e2d5a04683a170314ca35d068
Nagapruthvik S <92084826+Alister77@users.noreply.github.com>
2023-04-04 19:41:14 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,4 @@
+print("welcome to the brand name genertor")
+city= input("What's name of the city you grew up in?\n")
+pet=input("what's your pet's name?\n")
+print("your brand name could be:  " + city + " " + pet)
